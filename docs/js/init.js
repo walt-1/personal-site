6b0d71c73b3a545f0875c677ad581b8ccd601080
lukewalt <lukewdwrd.dev@gmail.com>
2019-02-07 11:33:27 -0600

    moved content to right side; fixed position for banner

--- docs/js/init.js
+++ docs/js/init.js
@@ -3,12 +3,6 @@ $('document').ready(function(){
   console.log('Welcome To My Site!');
 })
 
-let vph = $(window).height();
-
-$('#self-img').height(vph/ 1.2)
-$('#iphone').height(vph/ 3)
-
-
 AOS.init({
   duration: 2000,
 });
