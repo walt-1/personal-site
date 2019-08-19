bef9e527a680eab88e32c1670e769b783c57dfeb
Luke Woodward <luke.woodward@hcahealthcare.com>
2019-08-18 20:08:41 -0500

    updates menu toggle onclick function

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,11 @@
-
-$('document').ready(function(){
+$('document').ready(function () {
   console.log('Welcome To My Site!');
 })
+
+$('.navbar-collapse').click(() => {
+  $('.navbar-toggler').click();
+})
+
 
 AOS.init({
   duration: 2000,
