7edeb513f7513c928312b588a2d8db3df998a170
Luke Woodward <luke.woodward@hcahealthcare.com>
2020-04-26 19:45:43 -0500

    updates site with about info and project info; resume updates

--- docs/js/init.js
+++ docs/js/init.js
@@ -1,11 +1,18 @@
-$('document').ready(function () {
+$('document').ready(() => {
   console.log('Welcome To My Site!');
 })
 
-$('.navbar-collapse').click(() => {
-  $('.navbar-toggler').click();
+let width = window.innerWidth;
+
+$('window').resize(() => {
+  width = window.innerWidth;
 })
 
+if (width <= 991) {
+  $('.navbar li').click(() => {
+    $('.navbar-toggler').click();
+  })
+}
 
 AOS.init({
   duration: 2000,
